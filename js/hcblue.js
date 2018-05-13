4aed02ec92d25bfef1353e38abd63f4c7574eaf8
luisberumendesigncom <luis@berumendesign.com>
2018-05-13 02:09:52 -0600

    Updated dashboard

---
+++ js/hcblue.js
@@ -0,0 +1,31 @@
+Highcharts.theme = {
+    colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', 
+             '#FF9655', '#FFF263', '#6AF9C4'],
+    chart: {
+    },
+    title: {
+        style: {
+            color: '#333',
+            font: 'bold 16px sans-serif'
+        }
+    },
+    subtitle: {
+        style: {
+            color: '#666666',
+            font: 'bold 12px sans-serif'
+        }
+    },
+
+    legend: {
+        itemStyle: {
+            font: '9pt Trebuchet MS, Verdana, sans-serif',
+            color: 'black'
+        },
+        itemHoverStyle:{
+            color: 'gray'
+        }   
+    }
+};
+
+// Apply the theme
+Highcharts.setOptions(Highcharts.theme);
